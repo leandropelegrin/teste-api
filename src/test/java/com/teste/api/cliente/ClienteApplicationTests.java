08ddd97e6b89c31f547947ef5494654739c08aef
Leandro Pelegrin <leandropelegirn17@gmail.com>
2024-12-07 19:43:40 -0300

    projeto

---
+++ src/test/java/com/teste/api/cliente/ClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teste.api.cliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
